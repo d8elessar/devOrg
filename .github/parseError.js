046bcb6c54e183d45f4a11eae9504d6b8df5ee9d
Alexandre Valério <alexandre.valerio@worldit.pt>
2023-11-30 10:54:31 +0000

    test

--- .github/parseError.js
+++ .github/parseError.js
@@ -6,12 +6,20 @@ const data = JSON.parse(fs.readFileSync("./.github/stepOutput.txt", "utf8"));
 let markdown = "The deployment validation result is: \n";
 
 if (data.result && data.result.details && data.result.details.componentFailures) {
+    let lines = 0;
     markdown += "| componentType | fileName | lineNumber | problem |\n| --- | --- | --- | --- |\n";
+
     data.result.details.componentFailures.forEach((failure) => {
         if (!failure.success) {
             let problem = failure.problem.replace(/(\r\n|\n|\r)/gm, " ");
             let lineNumber = failure.lineNumber ? failure.lineNumber : "N/A";
             markdown += `| ${failure.componentType} | ${failure.fileName} | ${lineNumber} | ${problem} |\n`;
+
+            lines++;
+
+            if (lines >= 20) {
+                markdown += "(Error list truncated to the first 20 errors.)";
+            }
         }
     });
 } else {
