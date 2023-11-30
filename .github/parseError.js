e96597ac2cc7cd315ee9624ad71593b8f1258509
Alexandre Valério <alexandre.valerio@worldit.pt>
2023-11-30 10:27:02 +0000

    test

--- .github/parseError.js
+++ .github/parseError.js
@@ -5,13 +5,17 @@ const data = JSON.parse(fs.readFileSync("./.github/stepOutput.txt", "utf8"));
 
 let markdown = "\n| componentType | fileName | lineNumber | problem |\n| --- | --- | --- | --- |\n";
 
-data.result.details.componentFailures.forEach((failure) => {
-    if (!failure.success) {
-        let problem = failure.problem.replace(/(\r\n|\n|\r)/gm, " ");
-        let lineNumber = failure.lineNumber ? failure.lineNumber : "N/A";
-        markdown += `| ${failure.componentType} | ${failure.fileName} | ${lineNumber} | ${problem} |\n`;
-    }
-});
+if (data.result && data.result.details && data.result.details.componentFailures) {
+    data.result.details.componentFailures.forEach((failure) => {
+        if (!failure.success) {
+            let problem = failure.problem.replace(/(\r\n|\n|\r)/gm, " ");
+            let lineNumber = failure.lineNumber ? failure.lineNumber : "N/A";
+            markdown += `| ${failure.componentType} | ${failure.fileName} | ${lineNumber} | ${problem} |\n`;
+        }
+    });
+} else {
+    markdown = data.message;
+}
 
 // Write Markdown table to output file
 fs.writeFileSync("./.github/stepOutput.md", markdown);
