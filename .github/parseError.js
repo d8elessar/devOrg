1b52e7c3474c3182f272dbd7bffc4941ab07c671
Alexandre Valério <alexandre.valerio@worldit.pt>
2023-11-30 09:46:36 +0000

    test

--- .github/parseError.js
+++ .github/parseError.js
@@ -3,11 +3,12 @@ const fs = require("fs");
 // Read JSON data from input file
 const data = JSON.parse(fs.readFileSync("./.github/stepOutput.txt", "utf8"));
 
-let markdown = "| componentType | fileName | lineNumber | problem |\n| --- | --- | --- | --- |\n";
+let markdown = "\n| componentType | fileName | lineNumber | problem |\n| --- | --- | --- | --- |\n";
 
 data.result.details.componentFailures.forEach((failure) => {
     if (!failure.success) {
-        markdown += `| ${failure.componentType} | ${failure.fileName} | ${failure.lineNumber} | ${failure.problem} |\n`;
+        let problem = failure.problem.replace(/(\r\n|\n|\r)/gm, " ");
+        markdown += `| ${failure.componentType} | ${failure.fileName} | ${failure.lineNumber} | ${problem} |\n`;
     }
 });
 
